56b197adea3b96bf0916ad5c600948b82452e8d2
Md-Foisal <79102676+Md-Foisal@users.noreply.github.com>
2021-08-21 17:44:13 +0600

    first commit

---
+++ defualt.js
@@ -0,0 +1,76 @@
+
+    var total;
+    function total() {
+        var math = document.getElementById("math").value;
+        var physics = document.getElementById("physics").value;
+        var english = document.getElementById("english").value;
+        var bangla = document.getElementById("bangla").value;
+        total = +math + +physics + +english + +bangla;
+        document.getElementById("total").innerHTML = total;
+    }
+
+    var average;
+    function average() {
+      average = total / 4;
+      document.getElementById("average").innerHTML = average;
+    }
+    var grade;
+    function grade() {
+      if (average <= 100 && average >=80) {
+        document.getElementById("grade").innerHTML = "A+";
+      }
+      else if (average <= 79 && average >=70) {
+        document.getElementById("grade").innerHTML = "A";
+      }
+      else if (average <= 69 && average >=60) {
+        document.getElementById("grade").innerHTML = "A-";
+      }
+      else if (average <= 59 && average >=50) {
+        document.getElementById("grade").innerHTML = "B";
+      }
+      else if (average <= 49 && average >=40) {
+        document.getElementById("grade").innerHTML = "C";
+      }
+      else if (average <= 39 && average >=33) {
+        document.getElementById("grade").innerHTML = "D";
+      }
+      else {
+        document.getElementById("grade").innerHTML = "F";
+      }
+    }
+
+    // function submit() {
+    //   document.getElementById("total").innerHTML = total();
+
+    //   document.getElementById("average").innerHTML = average();
+
+    //   if (average <= 100 && average >=80) {
+    //     document.getElementById("grade").innerHTML = "A+";
+    //   }
+    //   else if (average <= 79 && average >=70) {
+    //     document.getElementById("grade").innerHTML = "A";
+    //   }
+    //   else if (average <= 69 && average >=60) {
+    //     document.getElementById("grade").innerHTML = "A-";
+    //   }
+    //   else if (average <= 59 && average >=50) {
+    //     document.getElementById("grade").innerHTML = "B";
+    //   }
+    //   else if (average <= 49 && average >=40) {
+    //     document.getElementById("grade").innerHTML = "C";
+    //   }
+    //   else if (average <= 39 && average >=33) {
+    //     document.getElementById("grade").innerHTML = "D";
+    //   }
+    //   else {
+    //     document.getElementById("grade").innerHTML = "F";
+    //   }
+
+document.querySelectorAll('input[type=number]').forEach(function(input){
+    input.addEventListener('input',limitRange);
+  });
+  
+  function limitRange() {
+    if (this.value < 0) this.value = 0;
+    if (this.value > 100) this.value = 100;
+  }
